c879c2bc2015a35e75a0a25ec15fd0fec1813e70
abraham-ship <abuzsein3@gmail.com>
2023-12-07 17:49:36 +0000

    updated task

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
 '''function to add float'''
-def add(a: float, b: float):
+def add(a: float, b: float) -> float:
     '''returns the sum of float as float'''
     return a + b
