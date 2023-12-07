400936ca8edf885d0a704fcd93200185bb1cf872
abraham-ship <abuzsein3@gmail.com>
2023-12-07 19:07:46 +0000

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 '''type annotated function  to return concatenated string'''
 
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     '''returns a concatenated string'''
     return str1 + str2
