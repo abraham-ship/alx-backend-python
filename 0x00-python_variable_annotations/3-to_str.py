eebb9dee8704376362d045e511b8067446148924
abraham-ship <abuzsein3@gmail.com>
2023-12-07 19:14:54 +0000

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''type-annotated function to returns a string representation of the float'''
+
+
+def to_str(n: float) -> str:
+    '''returns the string representation of a float'''
+    return str(n)
