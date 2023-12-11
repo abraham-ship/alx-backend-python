2ff0081b9c6f511d5a7e5bf7c7604f937e75a12d
abraham-ship <abuzsein3@gmail.com>
2023-12-11 14:32:34 +0000

    multiple coroutines

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,17 +3,17 @@
 import asyncio
 import random
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     '''
     function that takes in two int arguments and returns a sorted list
     in ascending order
     '''
     delays = []
     for _ in range(n):
-        delay = await task_random(max_delay)
+        delay = await task_wait_random(max_delay)
         delays.append(delay)
     await asyncio.sleep(max(delays))
     return sorted(delays)
