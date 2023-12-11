66b0399147dc1b805a08df4692a14604330f4cfc
abraham-ship <abuzsein3@gmail.com>
2023-12-11 10:03:15 +0000

    added annotation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay=10: int) -> float:
     '''
     waits for a random delay between 0 and max_delay seconds
     and returns it
