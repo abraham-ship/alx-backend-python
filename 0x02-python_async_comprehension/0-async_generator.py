98c054dfd56e9b039a32bc67774e1e5172a01534
abraham-ship <abuzsein3@gmail.com>
2023-12-12 12:06:59 +0000

    fixed order of operation in loop

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,6 +8,6 @@ from typing import List
 async def async_generator() -> List[float]:
     '''loops 10 times and yields a random number between 0 and 10'''
     for i in range(10):
-        n = random.uniform(0, 10)
         await asyncio.sleep(1)
+        n = random.uniform(0, 10)
         yield n
