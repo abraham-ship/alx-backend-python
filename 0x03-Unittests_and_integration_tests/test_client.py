8f7237bf16077676800592bd09c78d87cd254c86
abraham-ship <abuzsein3@gmail.com>
2024-01-08 13:18:17 +0000

    updated function args

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -26,3 +26,17 @@ class TestGithubOrgClient(unittest.TestCase):
                                               format(org=org_name))
 
         self.assertEqual(result, mock_response)
+
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=unittest.mock.PropertyMock)
+    def test_public_repos_url(self, mock_repos_url, mock_get_json):
+        '''Mocking a property'''
+        mock_payload = {"repos_url":
+                        "https://api.github.com/orgs/example/repos"}
+
+        with patch('client.GithubOrgClient.org', return_value=mock_payload):
+            github_org_client = GithubOrgClient("dummy_org")
+
+            result = github_org_client._public_repos_url
+
+            self.assertEqual(result, mock_payload["repos_url"])
